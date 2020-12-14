5cc8324f797127ef8e30a0801cfb4024c7961e32
user <user@USER-PC>
2020-12-14 10:58:14 -0500

    tablasSql

--- CapaDatos2/PersonaDAO.cs
+++ CapaDatos2/PersonaDAO.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace CapaDatos
 {
@@ -14,14 +15,21 @@ namespace CapaDatos
             //String cadenaConexcion = "Server="; database = Estudiantes; user id=sa; pwd=isa" ;
 
             //cadena de conexion utilizando usuario de windows
-            String cadenaConexcion = "Server="; database = Estudiantes; integrated security = true";
+            String cadenaConexcion = @"Server=USER-PC\SQLEXPRESS; database = Estudiantes; integrated security = true";
 
             //definir un objeto tipo Conexion para conectarnos con el servidor
             SqlConnection conexion = new SqlConnection(cadenaConexcion);
 
             //2. definir la operacion a realizar en el servidor
             //operacion : obtener todos los registros
-            string sql = "";
+            //sql(lenguaje estructurado de consultas)
+            string sql = "select cedula, apellidos, nombres, sexo, fechaNacimientos, correo, estarura, peso " + "from Personas";
+
+            //definir adaptador de datos: es un pueste que permite pasar los datos de la base de datos hacia el datatable
+            SqlDataAdapter ad = new SqlDataAdapter(sql, conexion);
+
+            //3. recuperamos los datos 
+            DataTable dt=new DataTable
         }
     }
 }
