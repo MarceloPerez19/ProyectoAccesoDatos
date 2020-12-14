b4185acda0a1e65aca019a1a0dc97e9f5faeecd4
user <user@USER-PC>
2020-12-14 10:27:59 -0500

    Agregar archivos de proyecto.

---
+++ CapaDatos/PersonaDAO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace CapaDatos
+{
+    public class PersonaDAO
+    {
+        public static DataTable getAll()
+        {
+            //1. definir y configurar la conexion con el motor de BDD
+            //cadena de conexion que utiliza la autentificacion que proporciona sql se
+            //String cadenaConexcion = "Server="; database = Estudiantes; user id=sa; pwd=isa" ;
+
+            //cadena de conexion utilizando usuario de windows
+            String cadenaConexcion = "Server="; database = Estudiantes; integrated security=true";
+
+            //definir un objeto tipo Conexion para conectarnos con el servidor
+            SqlConnection conexion = new SqlConnection(cadenaConexcion);
+        }
+    }
+}
---
+++ CapaDatos2/PersonaDAO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    class PersonaDAO
+    {
+        public static DataTable getAll()
+        {
+            //1. definir y configurar la conexion con el motor de BDD
+            //cadena de conexion que utiliza la autentificacion que proporciona sql se
+            //String cadenaConexcion = "Server="; database = Estudiantes; user id=sa; pwd=isa" ;
+
+            //cadena de conexion utilizando usuario de windows
+            String cadenaConexcion = "Server="; database = Estudiantes; integrated security = true";
+
+            //definir un objeto tipo Conexion para conectarnos con el servidor
+            SqlConnection conexion = new SqlConnection(cadenaConexcion);
+
+            //2. definir la operacion a realizar en el servidor
+            //operacion : obtener todos los registros
+            string sql = "";
+        }
+    }
+}
