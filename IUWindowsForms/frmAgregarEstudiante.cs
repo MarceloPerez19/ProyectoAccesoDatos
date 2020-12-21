350b8fc7cf99c103f547a2750e226019bf105226
user <user@USER-PC>
2020-12-21 09:53:25 -0500

    bambio2

--- IUWindowsForms/frmAgregarEstudiante.cs
+++ IUWindowsForms/frmAgregarEstudiante.cs
@@ -19,6 +19,27 @@ namespace IUWindowsForms
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
+            try
+            {
+                CapaDatos.Persona persona = new CapaDatos.Persona();
+                persona.Cedula = this.txtCedula.Text;
+                persona.Apellidos = this.txtApellido.Text;
+                persona.Nombres = this.txtNombre.Text;
+                persona.Sexo = this.cmbSexo.Text;
+                persona.fechaNacimiento = tdFechaNacimiento.Value;
+                persona.Correo = this.txtCorreo.Text;
+                persona.estatura = int.Parse(this.txtEstatura.Text);
+                persona.Peso = Decimal.Parse(this.txtPeso.Text);
+
+                int x = CapaDatos.PersonaDAO.crear(persona);
+                if (x > 0)
+                    MessageBox.Show("Persona agregado con exito");
+                else
+                    MessageBox.Show("No se pudo agregar el registro....");
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
             if(this.txtCedula.Text.Length==0)
             {
                 MessageBox.Show("Ingresa la cedula");
