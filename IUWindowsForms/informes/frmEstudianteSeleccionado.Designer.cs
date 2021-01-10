138a510a96af190892d7e6c42ec84aab3c5fd4b6
Aparicio <Aparicio@DESKTOP-P2GIT4H>
2021-01-10 13:28:55 -0500

    Tarea de informes incompleta

---
+++ IUWindowsForms/informes/frmEstudianteSeleccionado.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace IUWindowsForms.informes
+{
+    partial class frmEstudianteSeleccionado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbCedula = new System.Windows.Forms.ComboBox();
+            this.btnVerInforme = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Seleccione estudiante";
+            // 
+            // cmbCedula
+            // 
+            this.cmbCedula.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbCedula.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cmbCedula.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cmbCedula.FormattingEnabled = true;
+            this.cmbCedula.Location = new System.Drawing.Point(174, 22);
+            this.cmbCedula.Name = "cmbCedula";
+            this.cmbCedula.Size = new System.Drawing.Size(372, 21);
+            this.cmbCedula.TabIndex = 2;
+            // 
+            // btnVerInforme
+            // 
+            this.btnVerInforme.Location = new System.Drawing.Point(630, 25);
+            this.btnVerInforme.Name = "btnVerInforme";
+            this.btnVerInforme.Size = new System.Drawing.Size(75, 23);
+            this.btnVerInforme.TabIndex = 3;
+            this.btnVerInforme.Text = "Ver informe";
+            this.btnVerInforme.UseVisualStyleBackColor = true;
+            this.btnVerInforme.Click += new System.EventHandler(this.btnVerInforme_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(630, 76);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 4;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // frmEstudianteSeleccionado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 150);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnVerInforme);
+            this.Controls.Add(this.cmbCedula);
+            this.Controls.Add(this.label1);
+            this.Name = "frmEstudianteSeleccionado";
+            this.Text = "Informe por estudiante";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbCedula;
+        private System.Windows.Forms.Button btnVerInforme;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
---
+++ IUWindowsForms/informes/frmInformeEstudiante.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace IUWindowsForms.informes
+{
+    partial class frmInformeEstudiante
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.dsEstudiante = new IUWindowsForms.DS.dsEstudiante();
+            this.personasBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.personasTableAdapter = new IUWindowsForms.DS.dsEstudianteTableAdapters.PersonasTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dsEstudiante)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personasBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.personasBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "IUWindowsForms.informes.rptEstudiantes.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // dsEstudiante
+            // 
+            this.dsEstudiante.DataSetName = "dsEstudiante";
+            this.dsEstudiante.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // personasBindingSource
+            // 
+            this.personasBindingSource.DataMember = "Personas";
+            this.personasBindingSource.DataSource = this.dsEstudiante;
+            // 
+            // personasTableAdapter
+            // 
+            this.personasTableAdapter.ClearBeforeFill = true;
+            // 
+            // frmInformeEstudiante
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "frmInformeEstudiante";
+            this.Text = "frmInformeEstudiante";
+            this.Load += new System.EventHandler(this.frmInformeEstudiante_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dsEstudiante)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personasBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private DS.dsEstudiante dsEstudiante;
+        private System.Windows.Forms.BindingSource personasBindingSource;
+        private DS.dsEstudianteTableAdapters.PersonasTableAdapter personasTableAdapter;
+    }
+}
